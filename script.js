e7cd6f5e7f7ceaa065b8ea40e491310ea144dd8e
xlipepijn <xlipepijn@gmail.com>
2023-09-04 21:48:46 +0200

    add modal

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const mobileToggle = document.querySelector('[data-mobile-toggle]');
 const mobileMenu = document.querySelector('[data-mobile-menu]');
 const allLinks = [...document.querySelectorAll('a')];
+const allModalButtons = [...document.querySelectorAll('button[data-open-modal]')]
+const allModals = [...document.querySelectorAll('[data-modal]')]
+const bodySection = document.querySelector('body')
+
+function closeModal(currentModal) {
+    console.log(currentModal)
+    currentModal.close()
+    bodySection.dataset.modalState = "closed";
+
+}
+
+function openModal(triggeredModalId) {
+    let triggeredModal = allModals.find((modal) => modal.dataset.modal == triggeredModalId);
+    if (!triggeredModal) return (
+        console.log('nothing found :(', {triggeredModalId} )
+    )
+    bodySection.dataset.modalState = 'opened'
+    triggeredModal.show();
+}
+
+allModalButtons.forEach((modalButton) =>{
+    modalButton.addEventListener('click',(e) => openModal(e.currentTarget.dataset.openModal))
+})
+
+allModals.forEach((modal) => {
+    let currentCloseBtn = modal.querySelector('[data-close-modal]')
+    if(!currentCloseBtn) return console.log("no close btn found :(" , {modal})
+    currentCloseBtn.addEventListener("click", () => closeModal(modal));
+})
+
+
 function toggleMobileMenu() {
     if(mobileMenu.dataset.mobileMenu == 'closed') {
         mobileMenu.dataset.mobileMenu = 'open';
@@ -18,7 +49,6 @@ var flkty = new Flickity( '[data-slider]', {
 
 mobileToggle?.addEventListener('click', toggleMobileMenu);
 allLinks.forEach((link) => {
-    console.log(link);
     link.addEventListener('click',() => {
         mobileMenu.dataset.mobileMenu = "closed";
     })
